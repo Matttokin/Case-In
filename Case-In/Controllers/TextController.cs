7d19bca72026d9c5fb890fbd19300024ce6d9433
Токин Игорь Олегович <igor.tokin@nppltt.ru>
2021-03-18 20:06:51 +0300

    Create TextController.cs

---
+++ Case-In/Controllers/TextController.cs
@@ -0,0 +1,55 @@
+﻿using Case_In.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Case_In.Controllers
+{
+    public class TextController : ApiController
+    {
+        public BackJSON Post(string text)
+        {
+            try
+            {
+
+                BackJSON backJSON;
+                List<DataStruct> lds = new List<DataStruct>();
+                string outMessage = "";
+                switch (text)
+                {
+                    case "Привет": outMessage = "Приветствую Вас";
+                        break;
+
+                    default: outMessage = "Бот не умеет отвечать на такие сообщения";
+                        break;
+
+                }
+
+                lds.Add(new DataStruct()
+                {
+                    data = outMessage,
+                    type = BasicType.text
+                });
+                backJSON = new BackJSON()
+                {
+                    result = true,
+                    listData = lds,
+                    listCommand = null
+                };
+                return backJSON;
+            }
+            catch
+            {
+                return new BackJSON()
+                {
+                    result = false,
+                    errorMes = "Ошибка сервера"
+                };
+
+            }
+        }
+    }
+}
